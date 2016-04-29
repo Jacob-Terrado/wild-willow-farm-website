037be7abe8a8f36d2445e28babc8caeb8e71490e
Kimberly Ly <kimberly.ly95@gmail.com>
2016-04-28 17:23:09 -0700

    Beginning home page stuff
    Just the basics

---
+++ inc/header.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<nav class="navbar navbar-fixed-top">
+	<div class="container">
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Include all CSS files here -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/home.css" type="text/css">
+	</head>
+
+	<!-- Header -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		</div>
+	</nav>
+	<!-- End Header -->
+
+	<!-- Begin Body -->
+	<body>
+
+		<!-- Begin Carousel -->
+		<div id="news-carousel" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#news-carousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#news-carousel" data-slide-to="1"></li>
+		    <li data-target="#news-carousel" data-slide-to="2"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="https://placekitten.com/800/500" alt="cat">
+		      <div class="carousel-caption">
+		        hullo i am a cat lul
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="https://placekitten.com/800/500" alt="cat">
+		      <div class="carousel-caption">
+		        cat again
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="https://placekitten.com/800/500" alt="cat">
+		      <div class="carousel-caption">
+		        caaaaat
+		      </div>
+		    </div>
+		  </div>
+
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#news-carousel" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#news-carousel" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+		<!-- End Carousel -->
+
+		<!-- Begin 3 column content -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h2>Heading 1</h2>
+				</div>
+				<div class="col-md-4">
+					<h2>Heading 2</h2>
+				</div>
+				<div class="col-md-4">
+					<h2>Heading 3</h2>
+				</div>
+			</div>
+		</div>
+		<!-- End 3 column content -->
+		<!-- End Body -->
+		<!-- Begin Footer -->
+		<!-- End Footer -->
+		<!-- Include all JS here -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</body>
+</html>
